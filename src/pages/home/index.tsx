6f1c8b818b9ad0d855435521f44e6cb6f6243d60
Ismail Habibi Herman <ismail.habibi2104@gmail.com>
2021-11-01 23:39:50 +0700

    feat: update home page components, styles

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,45 +1,36 @@
 import * as React from 'react';
 import { useHistory } from 'react-router';
-import Button from 'components/buttons/Button';
+import CustomLink from 'components/links/CustomLink';
+import ButtonLink from 'components/links/ButtonLink';
 
 const Data = {
   projectLink: 'https://github.com/ismlhbb/react-ts-tw-starter',
-  projectName: 'React + Typescript + Tailwind Starter',
+  projectName: 'React + Typescript + Tailwind + Redux Starter',
   profileLink: 'https://ismailhabibi.netlify.app',
   profileName: 'Ismail Habibi Herman',
 };
 
 export default function Home() {
-  const history = useHistory();
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen'>
-      <a href={Data.projectLink}>
-        <h1>{Data.projectName}</h1>
-      </a>
-      <p className='mb-4'>
-        By <a href={Data.profileLink}>{Data.profileName}</a>
+    <div className='flex flex-col items-center justify-center text-center min-h-main'>
+      <h1>{Data.projectName}</h1>
+      <p className='mt-2 text-sm text-gray-700'>
+        🔥 An opinionated React + Typescript + Tailwind + Redux Starter.
       </p>
-      <p className='mb-4'>
-        <Button
-          className='mt-4'
-          variant='light'
-          onClick={() => history.push('/counter')}
-        >
-          Counter Page
-        </Button>
+      <p className='mt-2 text-sm text-gray-800'>
+        <CustomLink to={Data.projectLink}>See the repository</CustomLink>
       </p>
-      <p className='mb-4'>
-        <Button
-          className='mt-4'
-          variant='light'
-          onClick={() => history.push('/example')}
-        >
-          Example Page
-        </Button>
-      </p>
-      <footer className='absolute text-gray-300 bottom-2'>
-        © {new Date().getFullYear()}{' '}
-        <a href={Data.profileLink}>{Data.profileName}</a>
+
+      <ButtonLink className='mt-6' to='/example' variant='light'>
+        Counter example
+      </ButtonLink>
+      <ButtonLink className='mt-2' to='/counter' variant='light'>
+        Fetch example
+      </ButtonLink>
+
+      <footer className='absolute text-gray-600 bottom-2'>
+        © {new Date().getFullYear()} By{' '}
+        <CustomLink to={Data.profileLink}>{Data.profileName}</CustomLink>
       </footer>
     </div>
   );
